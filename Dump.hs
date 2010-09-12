d544121c0b529cafc97e1b31cef02571ac62c9b7
Fernando Serboncini <fserb@fserb.com.br>
2010-09-12 19:09:02 +0200

    loadDumpMap

--- Dump.hs
+++ Dump.hs
@@ -1,14 +1,25 @@
 module Dump
   (
-    constructName
+    constructName,
+    loadDumpMap
   ) where
 
+
+import Control.Monad
 import Data.String.Utils
 import Network.URL
-import qualified Data.Digest.Pure.SHA (sha1)
-import qualified Data.Map as Map
+import System.IO
+import System.Directory
+import System.FilePath
+import Data.Digest.Pure.SHA (sha1,Digest)
+import Data.Set (Set)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as BL8
+import qualified Data.Set as Set
 
 import Cache
+
+dumpPath = "dump"
 
 getBaseDomain :: String -> String
 getBaseDomain s =
@@ -30,3 +41,13 @@ constructName b t =
                  Right a -> "unknown"
   in (domain, ext)
 
+
+loadDumpMap :: String -> IO (Set Digest)
+loadDumpMap s =
+  do
+    c <- getDirectoryContents s
+    f <- filterM (doesFileExist . (s </>)) c
+    h <- mapM (getFile >=> (return . sha1)) f
+    return $ Set.fromList h
+  where getFile = flip openBinaryFile ReadMode . (s </>)
+                  >=> BL.hGetContents
