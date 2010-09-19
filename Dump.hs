a5baed86081401f4df49bbb5586ec999361a68d0
Fernando Serboncini <fserb@fserb.com.br>
2010-09-19 17:33:31 +0200

    split naming and dump

--- Dump.hs
+++ Dump.hs
@@ -6,7 +6,6 @@ module Dump
     dumpMap,
     makeDumpMap,
     notOnDumpMap,
-    getFilename
   ) where
 
 
@@ -15,8 +14,6 @@ import Data.String.Utils
 import Data.Char
 import Data.Bits
 import Data.Maybe
-import Network.URL
-import qualified Sound.TagLib as TL
 import System.IO
 import System.Directory
 import System.FilePath
@@ -28,7 +25,9 @@ import qualified Data.ByteString.Lazy.Char8 as BL8
 import qualified Data.Set as Set
 import Text.Printf
 import Text.Read
+
 import Cache
+import Naming
 
 dumpPath = "dump"
 dumpSHA1Map = ".dumpsha1"
@@ -43,25 +42,6 @@ instance Show Digest where
       paddedShowHex x xs = intToDigit (ord x `shiftR` 4)
                            : intToDigit (ord x .&. 0xf)
                            : xs
-
-
-getBaseDomain :: Block -> String
-getBaseDomain b =
-  case getURL b of
-    Right _ -> "unknown"
-    Left s -> case importURL s of
-                Nothing -> "unknown"
-                Just u -> let Absolute a = url_type u
-                              sp = split "." (host a)
-                          in sp !! (length sp - 2)
-
-
-makeFileExtension :: Block -> String
-makeFileExtension b =
-  case contentType b of
-    "audio/mpeg" -> "mp3"
-    "video/x-flv" -> "flv"
-    _ -> fail "Unknown mime: " ++ (contentType b)
 
 
 loadDumpMapFromDir :: String -> IO (Set Digest)
@@ -121,64 +101,12 @@ notOnDumpMap s b =
     return $ Set.notMember (Digest . bytestringDigest $ sha1 c) s
 
 
-nextAvailableName :: String -> String -> IO String
-nextAvailableName n s =
-  do
-    c <- getDirectoryContents s
-    f <- filterM (doesFileExist . (s </>)) c
-    let names = (map dropExtension f)
-    let numbers = mapMaybe filterNamesOut names
-    let next = 1 + foldl max 0 numbers
-    return $ n ++ "-" ++ show next
-  where filterNamesOut x = if startswith (n ++ "-") x
-                           then Just $ read $ drop (length n + 1) x
-                           else Nothing
-
-
-makeFilename :: Block -> String -> IO String
-makeFilename b s =
-  do
-    let domain = getBaseDomain b
-    let ext = makeFileExtension b
-    basename <- nextAvailableName domain s
-    return $ basename ++ "." ++ ext
-
-
-setAlbum :: Block -> String -> IO ()
-setAlbum b s =
-  do
-    let d = getBaseDomain b
-    tag <- TL.open s >>= maybe (return Nothing) TL.tag
-    case tag of
-      Nothing -> return ()
-      Just t -> TL.setAlbum t d
-
-
-getFilename :: Block -> String -> IO (Maybe String)
-getFilename b s =
-  do
-    tag <- TL.open s >>= maybe (return Nothing) TL.tag
-    case tag of
-      Nothing -> return Nothing
-      Just t -> do
-               artist <- TL.artist t
-               title <- TL.title t
-               let ext = makeFileExtension b
-               return $ if (and [(length artist > 0),
-                                 (length title > 0)])
-                        then Just $ printf "%s - %s.%s" artist title ext
-                        else Nothing
-
-
 dumpBlock :: Block -> IO ()
 dumpBlock b =
   do
     content <- loadAddr $ contentAddress b
     let temp = tempFilename ++ "." ++ makeFileExtension b
     BL8.writeFile temp content
-    g <- getFilename b temp
-    filename <- case g of
-                  Nothing -> makeFilename b dumpPath
-                  Just n  -> setAlbum b temp >> return n
+    filename <- blockFilename dumpPath b temp
     renameFile temp $ dumpPath </> filename
 
---
+++ Naming.hs
@@ -0,0 +1,93 @@
+module Naming
+  (
+   blockFilename,
+   makeFileExtension
+  ) where
+
+import Control.Monad
+import Network.URL
+import Data.String.Utils
+import System.Directory
+import System.FilePath
+import System.FilePath.Posix
+import qualified Sound.TagLib as TL
+import Data.Maybe
+import Text.Printf
+
+import Cache
+
+getBaseDomain :: Block -> String
+getBaseDomain b =
+  case getURL b of
+    Right _ -> "unknown"
+    Left s -> case importURL s of
+                Nothing -> "unknown"
+                Just u -> let Absolute a = url_type u
+                              sp = split "." (host a)
+                          in sp !! (length sp - 2)
+
+
+makeFileExtension :: Block -> String
+makeFileExtension b =
+  case contentType b of
+    "audio/mpeg" -> "mp3"
+    "video/x-flv" -> "flv"
+    _ -> fail "Unknown mime: " ++ (contentType b)
+
+
+nextAvailableName :: String -> String -> IO String
+nextAvailableName n s =
+  do
+    c <- getDirectoryContents s
+    f <- filterM (doesFileExist . (s </>)) c
+    let names = (map dropExtension f)
+    let numbers = mapMaybe filterNamesOut names
+    let next = 1 + foldl max 0 numbers
+    return $ n ++ "-" ++ show next
+  where filterNamesOut x = if startswith (n ++ "-") x
+                           then Just $ read $ drop (length n + 1) x
+                           else Nothing
+
+
+makeFilename :: Block -> String -> IO String
+makeFilename b s =
+  do
+    let domain = getBaseDomain b
+    let ext = makeFileExtension b
+    basename <- nextAvailableName domain s
+    return $ basename ++ "." ++ ext
+
+
+setAlbum :: Block -> String -> IO ()
+setAlbum b s =
+  do
+    let d = getBaseDomain b
+    tag <- TL.open s >>= maybe (return Nothing) TL.tag
+    case tag of
+      Nothing -> return ()
+      Just t -> TL.setAlbum t d
+
+
+getFilename :: Block -> String -> IO (Maybe String)
+getFilename b s =
+  do
+    tag <- TL.open s >>= maybe (return Nothing) TL.tag
+    case tag of
+      Nothing -> return Nothing
+      Just t -> do
+               artist <- TL.artist t
+               title <- TL.title t
+               let ext = makeFileExtension b
+               return $ if (and [(length artist > 0),
+                                 (length title > 0)])
+                        then Just $ printf "%s - %s.%s" artist title ext
+                        else Nothing
+
+
+blockFilename :: String -> Block -> String -> IO String
+blockFilename path b temp =
+  do
+    g <- getFilename b temp
+    case g of
+      Nothing -> makeFilename b path
+      Just n  -> setAlbum b temp >> return n
